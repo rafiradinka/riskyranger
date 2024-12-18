5d62d1239313f594e0c0058d2458f529ad30de0e
rafiradinka <rafi.radinka@gmail.com>
2024-12-18 13:53:53 +0700

    merapikan susunan

--- models/m_manageUser.php
+++ models/m_manageUser.php
@@ -26,16 +26,33 @@ class ManUser {
     public function tambah($id_user, $username, $level, $unit_terkait, $password) {
         $db = $this->connection;
         $hashedPassword = md5($password);
-        $stmt = $db->prepare("INSERT INTO tb_user (username, level, unit_terkait, password) VALUES (?, ?, ?, ?)");
-        $stmt->bind_param('ssss', $username, $level, $unit_terkait, $hashedPassword);
+        $stmt = $db->prepare("INSERT INTO tb_user 
+            (username, level, unit_terkait, password) 
+            VALUES (?, ?, ?, ?)");
+        $stmt->bind_param('ssss', 
+            $username, 
+            $level, 
+            $unit_terkait, 
+            $hashedPassword
+        );
         $stmt->execute() or die($db->error);
         $stmt->close();
     }
 
     public function edit($id_user, $username, $level, $unit_terkait){
         $db = $this->connection;
-        $stmt = $db->prepare("UPDATE tb_user SET username = ?, level = ?, unit_terkait = ? WHERE id_user = ?");
-        $stmt->bind_param('sssi', $username, $level, $unit_terkait, $id_user);
+        $stmt = $db->prepare("UPDATE tb_user SET 
+            username = ?, 
+            level = ?, 
+            unit_terkait = ? 
+            WHERE id_user = ?"
+        );
+        $stmt->bind_param('sssi', 
+            $username, 
+            $level, 
+            $unit_terkait, 
+            $id_user
+        );
         $stmt->execute() or die($db->error);
         $stmt->close();
     }
