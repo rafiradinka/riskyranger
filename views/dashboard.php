248be209b48749e672248a399f594f90112a4344
rafiradinka <rafi.radinka@gmail.com>
2024-12-18 14:53:05 +0700

    merapikan kode

--- views/dashboard.php
+++ views/dashboard.php
@@ -1,12 +1,14 @@
 <?php include("../template/_header.php");?>
-
-    <div class="row">
-        <div class="col-lg-12">
-            <h1>Dashboard</h1>
-            <h3>Selamat Datang, <?= $level_tampilan?></h3>
-            <a href="#menu-toggle" class="btn btn-default" id="menu-toggle">Toggle Menu</a>
+    <div id="page-content-wrapper">
+      <div class="container-fluid">
+        <div class="row">
+            <div class="col-lg-12">
+                <h1>Dashboard</h1>
+                <h3>Selamat Datang, <?= $level_tampilan?></h3>
+                <a href="#menu-toggle" class="btn btn-default" id="menu-toggle">Toggle Menu</a>
+            </div>
         </div>
+      </div>
     </div>
-
 
 <?php include("../template/_footer.php");?>
